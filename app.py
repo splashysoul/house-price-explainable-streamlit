6f34aec66df63cde5b8541379bc36cdacb9095a3
Andrew Huang (Cheng Chiang) <33182390+epadam@users.noreply.github.com>
2021-10-04 05:18:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,22 +7,28 @@ from sklearn.model_selection import cross_val_score
 from collections import Counter
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+from sklearn.linear_model import LinearRegression
+
+
 import streamlit as st
 import shap
+import LIME
 
 
-boston_dataset = load_boston()
-dataset = pd.DataFrame(boston_dataset.data, columns = boston_dataset.feature_names)
-dataset['MEDV'] = boston_dataset.target
 
-X = dataset.iloc[:, 0:13].values
-y = dataset.iloc[:, 13].values.reshape(-1,1)
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 25)
 
 st.title('Explainable Machine Learning')
 
 task = st.sidebar.selectbox('Select task', ('Boston House', 'Heart Disease'))
+
+if task == 'Boston House':
+  boston_dataset = load_boston()
+  dataset = pd.DataFrame(boston_dataset.data, columns = boston_dataset.feature_names)
+  dataset['MEDV'] = boston_dataset.target
+  X = dataset.iloc[:, 0:13].values
+  y = dataset.iloc[:, 13].values.reshape(-1,1)
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 25)
 
 if task == 'Boston House':
   
@@ -35,6 +41,18 @@ if task == 'Boston House':
     plt.xticks(range(len(corr.columns)), corr.columns)
     plt.yticks(range(len(corr.columns)), corr.columns)
     st.write(fig)
+    sns.pairplot(dataset)
+    plt.show()
   
   if steps == 'Model Prediction and Explain':
     model = st.sidebar.selectbox('Select model', ('linear regression', 'XGBoost', ''))
+    if model == 'linear regression':
+      regressor_linear = LinearRegression()
+      regressor_linear.fit(X_train, y_train)
+
+cv_linear = cross_val_score(estimator = regressor_linear, X = X_train, y = y_train, cv = 10)
+y_pred_linear_train = regressor_linear.predict(X_train)
+r2_score_linear_train = r2_score(y_train, y_pred_linear_train)
+y_pred_linear_test = regressor_linear.predict(X_test)
+r2_score_linear_test = r2_score(y_test, y_pred_linear_test)
+rmse_linear = (np.sqrt(mean_squared_error(y_test, y_pred_linear_test)))
